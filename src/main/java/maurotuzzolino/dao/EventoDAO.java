d63581a94293beb6c64739fb8cc53b767d282ae6
MauroTuzzolino <tuzzolinomauro.prog05@gmail.com>
2025-07-03 15:06:03 +0200

    Aggiunta dei metodi ad EvenotoDAO
    getConcertiInStreaming
    getConcertiPerGenere

--- src/main/java/maurotuzzolino/dao/EventoDAO.java
+++ src/main/java/maurotuzzolino/dao/EventoDAO.java
@@ -2,7 +2,12 @@ package maurotuzzolino.dao;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.TypedQuery;
+import maurotuzzolino.entities.Concerto;
 import maurotuzzolino.entities.Evento;
+import maurotuzzolino.entities.GenereConcerto;
+
+import java.util.List;
 
 public class EventoDAO {
     private EntityManager entityManager;
@@ -21,5 +26,19 @@ public class EventoDAO {
 
     public Evento getById(long id) {
         return entityManager.find(Evento.class, id);
+    }
+
+    public List<Concerto> getConcertiInStreaming(boolean inStreaming) {
+        TypedQuery<Concerto> query = entityManager.createQuery(
+                "SELECT c FROM Concerto c WHERE c.inStreaming = :streaming", Concerto.class);
+        query.setParameter("streaming", inStreaming);
+        return query.getResultList();
+    }
+
+    public List<Concerto> getConcertiPerGenere(GenereConcerto genere) {
+        TypedQuery<Concerto> query = entityManager.createQuery(
+                "SELECT c FROM Concerto c WHERE c.genere = :genere", Concerto.class);
+        query.setParameter("genere", genere);
+        return query.getResultList();
     }
 }
